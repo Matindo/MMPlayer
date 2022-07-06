52d2c020cf434bb982a57cf044fc698deba89a85
Ichybell <98348980+ichybell@users.noreply.github.com>
2022-07-06 09:44:33 +0300

    Initial Setup V1.0

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <navbar></navbar>
+    <router-view/>
+  </div>
+</template>
+<script>
+import Navbar from '@/components/Navbar.vue'
+export default {
+  components: { Navbar }
+}
+</script>
+
+<style lang="scss">
+@import '../src/assets/styles/custom.scss';
+body {
+  background: $body-bg center center fixed;
+  color: $body-color;
+}
+</style>
