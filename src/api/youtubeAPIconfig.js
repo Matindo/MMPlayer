f2c23eef7ab246197c111a270065dc728662baf7
owinOchiaki <owinophil321@gmail.com>
2022-07-13 13:15:54 +0300

    added api folder

---
+++ src/api/youtubeAPIconfig.js
@@ -0,0 +1,4 @@
+export default {
+  URL: 'https://youtube.googleapis.com/youtube/v3/search',
+  API_KEY: 'AIzaSyBdTZwLIYfcNVTOZrSJv0Aij8X4bHVYeDE'
+}
---
+++ src/views/VideoView.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-container fluid>
+    <b-row></b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'VideoView',
+  data: function () {
+    return {
+      video: this.$route.params.video
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
