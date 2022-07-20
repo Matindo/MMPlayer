3f5f38ca5a7808b1251681b63003ffecf0fe105f
owinOchiaki <owinophil321@gmail.com>
2022-07-20 09:55:05 +0300

    Added localStorage service to store data temporarily

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
 
+const vuexLocal = new VuexPersistence({
+  supportCircular: true,
+  storage: window.localStorage
+})
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -109,5 +114,6 @@ export default new Vuex.Store({
     }
   },
   modules: {
-  }
+  },
+  plugins: [vuexLocal.plugin]
 })
